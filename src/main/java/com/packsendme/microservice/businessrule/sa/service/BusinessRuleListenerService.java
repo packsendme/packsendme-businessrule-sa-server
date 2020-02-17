7fd7ec372436b4843d5d3f5158c9a8826172bb4b
ricardomarzochi <ricardomarzochi@gmail.com>
2020-02-17 11:15:18 +0100

    NewVersion-ChangeProperties

--- src/main/java/com/packsendme/microservice/businessrule/sa/service/BusinessRuleListenerService.java
+++ src/main/java/com/packsendme/microservice/businessrule/sa/service/BusinessRuleListenerService.java
@@ -5,11 +5,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class BusinessRuleListenerService {
-
+/*
 	@KafkaListener(topics = "${configuration:topic_simulation_southamerica}")
     public void receive(String data) {
 		System.out.println(" ---------------------------- ");
 		System.out.println(" BusinessRuleListenerService "+ data);
 		System.out.println(" ---------------------------- ");
 	}
+	*/
 }
