f7a2817581434cf6097a6b3ac2e0bf82fb45be3d
ricardomarzochi <ricardomarzochi@gmail.com>
2020-02-23 19:59:11 +0100

    NewVersion-001-New

--- src/main/java/com/packsendme/microservice/businessrule/sa/service/ProduceSenderBusinessRuleService.java
+++ src/main/java/com/packsendme/microservice/businessrule/sa/service/ProduceSenderBusinessRuleService.java
@@ -1,26 +1,16 @@
 package com.packsendme.microservice.businessrule.sa.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.kafka.support.SendResult;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.packsendme.lib.businessrule.model.BusinessRulesModel;
-import com.packsendme.lib.common.constants.HttpExceptionPackSend;
-import com.packsendme.lib.common.response.Response;
 import com.packsendme.microservice.businessrule.sa.config.Configuration;
  
 @Service
-public class BusinessRuleSenderService {
+public class ProduceSenderBusinessRuleService {
 	
 	
 	@Autowired
@@ -29,6 +19,9 @@ public class BusinessRuleSenderService {
 	@Autowired
 	private Configuration configuration;
 	
+	
+	
+	/*
 	public ResponseEntity<?> sendRuleSA(BusinessRulesModel businessrule) {		
 		ObjectMapper mapper = new ObjectMapper();
 		Response<String> responseObj = null;
@@ -68,7 +61,7 @@ public class BusinessRuleSenderService {
 		                .setHeader(KafkaHeaders.PARTITION_ID, 1)
 		                .build();
 			this.kafkaTemplate.send(message); */
-	        
+	    /*    
 			responseObj = new Response<String>(0,HttpExceptionPackSend.BUSINESS_RULE.getAction(), "ruleJson");
 			return new ResponseEntity<>(responseObj, HttpStatus.ACCEPTED);
 			
@@ -77,7 +70,25 @@ public class BusinessRuleSenderService {
 			responseObj = new Response<String>(0,HttpExceptionPackSend.FAIL_EXECUTION.getAction(), null);
 			return new ResponseEntity<>(responseObj, HttpStatus.INTERNAL_SERVER_ERROR);
 
-		}
-	}
+		} 
 	
+	} */
+	
+	public void sendMessage(String message) {
+		ListenableFuture<SendResult<String, String>> future = 
+		kafkaTemplate.send("topicBusinessRuleSouthAmericaDev", "ALICIA");
+		
+		future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+            	System.out.println("Sent message=[" + message + "] with offset=[" + result.getRecordMetadata().offset() + "]");            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+            	System.out.println("Unable to send message=["+ message + "] due to : " + ex.getMessage());
+            }
+        });
+	}
+
 }
