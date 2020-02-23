6555a7bf9aa618d8bb8af37222ac50fe98f030a9
ricardomarzochi <ricardomarzochi@gmail.com>
2020-02-23 21:24:10 +0100

    NewVersion-001-NewProduce-CHANGE-5

--- src/main/java/com/packsendme/microservice/businessrule/sa/config/ProduceConfig.java
+++ src/main/java/com/packsendme/microservice/businessrule/sa/config/ProduceConfig.java
@@ -32,8 +32,8 @@ public class ProduceConfig {
 		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class);
 		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class);
 		configProps.put(ProducerConfig.LINGER_MS_CONFIG,10);
-		configProps.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG,30000);
-		configProps.put(ProducerConfig.BATCH_SIZE_CONFIG, 9000);
+		configProps.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG,50000);
+		configProps.put(ProducerConfig.BATCH_SIZE_CONFIG, 5000);
 
 		return new DefaultKafkaProducerFactory<>(configProps);
 		
