f9341e63e9d2de46a25d3a6281b3fe5af8e4c1c8
ricardomarzochi@gmail.com <ricardomarzochi@gmail.com>
2020-02-24 17:17:05 +0100

    NewParameterTesting3

--- src/main/java/com/packsendme/microservice/businessrule/sa/config/ProduceConfig.java
+++ src/main/java/com/packsendme/microservice/businessrule/sa/config/ProduceConfig.java
@@ -31,7 +31,7 @@ public class ProduceConfig {
 		configProps.put(ProducerConfig.ACKS_CONFIG,"1");
 		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class);
 		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class);
-		configProps.put(ProducerConfig.LINGER_MS_CONFIG,10);
+		configProps.put(ProducerConfig.LINGER_MS_CONFIG,10000);
 		configProps.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG,20000);
 		configProps.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG,50000);
 		configProps.put(ProducerConfig.BATCH_SIZE_CONFIG, 0);
