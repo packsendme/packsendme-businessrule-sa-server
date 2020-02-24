525c4ecef29d418960b618ba0046f042c2e25974
ricardomarzochi@gmail.com <ricardomarzochi@gmail.com>
2020-02-24 17:13:11 +0100

    NewParameterTestingII

--- src/main/java/com/packsendme/microservice/businessrule/sa/config/ProduceConfig.java
+++ src/main/java/com/packsendme/microservice/businessrule/sa/config/ProduceConfig.java
@@ -33,8 +33,8 @@ public class ProduceConfig {
 		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class);
 		configProps.put(ProducerConfig.LINGER_MS_CONFIG,10);
 		configProps.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG,20000);
-		configProps.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG,100000);
-		configProps.put(ProducerConfig.BATCH_SIZE_CONFIG, 1000);
+		configProps.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG,50000);
+		configProps.put(ProducerConfig.BATCH_SIZE_CONFIG, 0);
 
 		return new DefaultKafkaProducerFactory<>(configProps);
 		
