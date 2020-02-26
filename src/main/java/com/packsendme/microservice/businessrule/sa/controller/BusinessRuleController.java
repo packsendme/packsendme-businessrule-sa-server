7bc22ef02a62bc2763302b7d0d7a882bbd05255b
ricardomarzochi@gmail.com <r.libera.marzochi@accenture.com>
2020-02-26 17:08:16 +0100

    NewVersion-Consumer

--- src/main/java/com/packsendme/microservice/businessrule/sa/controller/BusinessRuleController.java
+++ src/main/java/com/packsendme/microservice/businessrule/sa/controller/BusinessRuleController.java
@@ -4,31 +4,46 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.packsendme.lib.businessrule.model.BusinessRulesModel;
+import com.packsendme.microservice.businessrule.sa.service.ConsumerSenderBRService;
 import com.packsendme.microservice.businessrule.sa.service.ProduceSenderBRService;
-
 
 @RestController
 public class BusinessRuleController {
 
 	
 	@Autowired
-	private ProduceSenderBRService businessRuleSA; 
-
+	private ProduceSenderBRService produceRuleSA; 
+	
+	@Autowired
+	private ConsumerSenderBRService consumerRuleSA; 
 	
 	@PostMapping("/businessrule/sa")
 	public ResponseEntity<?> postBusinessRuleSouthAmerica(
 			@Validated @RequestBody BusinessRulesModel brObject) {		
 		try {
-			return businessRuleSA.sendMessage(brObject);
+			return produceRuleSA.sendMessage(brObject);
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 			return new ResponseEntity<>(brObject, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
+	
+	@GetMapping("/businessrule/sa")
+	public ResponseEntity<?> getBusinessRuleSouthAmerica() {		
+		try {
+			System.out.println(" getBusinessRuleSouthAmerica ");
+			return consumerRuleSA.consumerMessage();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
 }
--- src/main/java/com/packsendme/microservice/businessrule/sa/service/ConsumerSenderBRService.java
+++ src/main/java/com/packsendme/microservice/businessrule/sa/service/ConsumerSenderBRService.java
@@ -1,15 +1,36 @@
 package com.packsendme.microservice.businessrule.sa.service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.packsendme.lib.common.constants.HttpExceptionPackSend;
+import com.packsendme.lib.common.response.Response;
 
 @Service
 public class ConsumerSenderBRService {
-/*
-	@KafkaListener(topics = "${configuration:topic_simulation_southamerica}")
+
+	private String msg;
+	
+	@KafkaListener(topics = "${topicConf:topicBusinessRuleSouthAmericaDev}")
     public void receive(String data) {
+		this.msg = data;
 		System.out.println(" ---------------------------- ");
 		System.out.println(" BusinessRuleListenerService "+ data);
 		System.out.println(" ---------------------------- ");
 	}
-	*/
+	
+	public ResponseEntity<?> consumerMessage() {
+		Response<String> responseObj = null;
+		try {
+			responseObj = new Response<String>(0,HttpExceptionPackSend.BUSINESS_RULE.getAction(), msg);
+			return new ResponseEntity<>(responseObj, HttpStatus.ACCEPTED);
+		} catch (Exception e) {
+			responseObj = new Response<String>(0,HttpExceptionPackSend.FAIL_EXECUTION.getAction(), null);
+			return new ResponseEntity<>(responseObj, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
 }
