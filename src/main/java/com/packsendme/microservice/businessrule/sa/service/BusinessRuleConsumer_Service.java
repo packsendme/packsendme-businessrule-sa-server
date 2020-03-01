ad2295303497bf0b5b6a224a5648dd877fb62187
ricardomarzochi@gmail.com <r.libera.marzochi@accenture.com>
2020-03-01 20:00:48 +0100

    NewVersion-00

--- src/main/java/com/packsendme/microservice/businessrule/sa/service/BusinessRuleConsumer_Service.java
+++ src/main/java/com/packsendme/microservice/businessrule/sa/service/BusinessRuleConsumer_Service.java
@@ -1,5 +1,6 @@
 package com.packsendme.microservice.businessrule.sa.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -7,12 +8,17 @@ import org.springframework.stereotype.Service;
 
 import com.packsendme.lib.common.constants.HttpExceptionPackSend;
 import com.packsendme.lib.common.response.Response;
+import com.packsendme.microservice.sa.component.MultiMethodsConsumer_Component;
 
 @Service
 public class BusinessRuleConsumer_Service {
 
 	private String businessruleJson;
 	
+	@Autowired
+	private MultiMethodsConsumer_Component multiConsumer; 
+	
+	/*
 	//@KafkaListener(topics = "${topicConfig:topic_businessrule_sa}")
 	@KafkaListener(topics = "${packsendme-businessrule-sa-server.topic_businessrule_sa}")
     public void receive(String data) {
@@ -20,11 +26,13 @@ public class BusinessRuleConsumer_Service {
 		System.out.println(" ---------------------------- ");
 		System.out.println(" BusinessRuleListenerService "+ data);
 		System.out.println(" ---------------------------- ");
-	}
+	}*/
 	
 	public ResponseEntity<?> consumerMessage() {
 		Response<String> responseObj = null;
 		try {
+			System.out.println(" receiveRoadyay "+ multiConsumer.topic_msg);
+			
 			responseObj = new Response<String>(0,HttpExceptionPackSend.BUSINESS_RULE.getAction(), businessruleJson);
 			return new ResponseEntity<>(responseObj, HttpStatus.ACCEPTED);
 		} catch (Exception e) {
