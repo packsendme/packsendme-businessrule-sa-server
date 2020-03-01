8f004525c760fa5b61e950f126a1b240af1b2b61
ricardomarzochi@gmail.com <r.libera.marzochi@accenture.com>
2020-03-01 19:11:25 +0100

    NewVersion-5500

--- src/main/java/com/packsendme/microservice/businessrule/sa/service/BusinessRuleConsumer_Service.java
+++ src/main/java/com/packsendme/microservice/businessrule/sa/service/BusinessRuleConsumer_Service.java
@@ -13,7 +13,8 @@ public class BusinessRuleConsumer_Service {
 
 	private String businessruleJson;
 	
-	@KafkaListener(topics = "${topicConfig:topic_businessrule_sa}")
+	//@KafkaListener(topics = "${topicConfig:topic_businessrule_sa}")
+	@KafkaListener(id = "BusinessRule", topics = "#{'${topicConfig:topic_businessrule_sa,BusinessRule}'.split(',')}")
     public void receive(String data) {
 		this.businessruleJson = data;
 		System.out.println(" ---------------------------- ");
