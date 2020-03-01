7babf42af92c828b21a735228415d56d07df526c
ricardomarzochi@gmail.com <r.libera.marzochi@accenture.com>
2020-03-01 19:38:33 +0100

    NewVersion-001

---
+++ src/main/java/com/packsendme/microservice/sa/component/MultiMethodsConsumer_Component.java
@@ -0,0 +1,10 @@
+package com.packsendme.microservice.sa.component;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+//@KafkaListener(id = "multiConsumers", topics = { "foos", "bars" })
+public class MultiMethodsConsumer_Component {
+
+}
