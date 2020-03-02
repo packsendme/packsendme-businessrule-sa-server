3b93a0cf3f0bc62b5a9915ab6a2449a50c72f887
ricardomarzochi@gmail.com <r.libera.marzochi@accenture.com>
2020-03-02 21:48:02 +0100

    NewVersion-Success-0909

--- src/main/java/com/packsendme/microservice/businessrule/sa/config/Consumer_Config.java
+++ src/main/java/com/packsendme/microservice/businessrule/sa/config/Consumer_Config.java
@@ -1,9 +1,22 @@
 package com.packsendme.microservice.businessrule.sa.config;
 
-//@EnableKafka
-//@Configuration
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+@EnableKafka
+@Configuration
 public class Consumer_Config {
-	/*
+	
 	@Value("${spring.kafka.consumer.bootstrap-servers}")
     private String bootstrapServers;
 
@@ -11,7 +24,7 @@ public class Consumer_Config {
 	public ConsumerFactory<String, String> consumerFactory() {
 		Map<String, Object> props = new HashMap<>();
 	    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-	    props.put(ConsumerConfig.GROUP_ID_CONFIG, "groupId");
+	   // props.put(ConsumerConfig.GROUP_ID_CONFIG, "groupId");
 	    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 	    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 	    return new DefaultKafkaConsumerFactory<>(props);
@@ -24,5 +37,5 @@ public class Consumer_Config {
 	    factory.setConsumerFactory(consumerFactory());
 	    return factory;
 	}
-	*/
+	
 }
