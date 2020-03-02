81be24b56dc3f068a90bf827dc0bcded3c22f914
ricardomarzochi@gmail.com <r.libera.marzochi@accenture.com>
2020-03-02 23:29:10 +0100

    NewVersion-Success-0001

--- src/main/java/com/packsendme/microservice/businessrule/sa/config/Consumer_Config.java
+++ src/main/java/com/packsendme/microservice/businessrule/sa/config/Consumer_Config.java
@@ -1,10 +1,23 @@
 package com.packsendme.microservice.businessrule.sa.config;
 
-//@EnableKafka
-//@Configuration
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+@EnableKafka
+@Configuration
 public class Consumer_Config {
 	
-	/*
+	
 	@Value("${spring.kafka.consumer.bootstrap-servers}")
     private String bootstrapServers;
 
@@ -24,8 +37,5 @@ public class Consumer_Config {
 	    ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
 	    factory.setConsumerFactory(consumerFactory());
 	    return factory;
-	}
-	
-	*/
-	
+	}	
 }
