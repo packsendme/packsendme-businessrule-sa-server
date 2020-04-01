a4dab42eef6e2da20276d4bcf02b501b2f677708
ricardomarzochi@gmail.com <r.libera.marzochi@accenture.com>
2020-04-01 10:38:26 +0200

    NewVersion-Release/0.2.0

--- src/main/java/com/packsendme/microservice/sa/businessrule/application/MicroserviceBusinessRuleServer_SA_Application.java
+++ src/main/java/com/packsendme/microservice/sa/businessrule/application/MicroserviceBusinessRuleServer_SA_Application.java
@@ -3,9 +3,13 @@ package com.packsendme.microservice.sa.businessrule.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.packsendme.microservice.sa.businessrule.controller.RoadwayBRE_Controller;
 
 @SpringBootApplication
 @EnableEurekaClient
+@ComponentScan(basePackageClasses = RoadwayBRE_Controller.class)
 public class MicroserviceBusinessRuleServer_SA_Application {
 
 	public static void main(String[] args) {
