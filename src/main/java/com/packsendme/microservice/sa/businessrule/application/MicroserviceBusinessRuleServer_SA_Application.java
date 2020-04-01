897e490f2a78efa80ce26af3882c8fd1002fac53
ricardomarzochi@gmail.com <r.libera.marzochi@accenture.com>
2020-04-01 11:11:09 +0200

    NewVersion-Release/0.2.0

--- src/main/java/com/packsendme/microservice/sa/businessrule/application/MicroserviceBusinessRuleServer_SA_Application.java
+++ src/main/java/com/packsendme/microservice/sa/businessrule/application/MicroserviceBusinessRuleServer_SA_Application.java
@@ -3,11 +3,10 @@ package com.packsendme.microservice.sa.businessrule.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
-@ComponentScan(basePackages = "com.packsendme.microservice.sa.businessrule.controller")
+//@ComponentScan(basePackages = "com.packsendme.microservice.sa.businessrule.controller")
 public class MicroserviceBusinessRuleServer_SA_Application {
 
 	public static void main(String[] args) {
