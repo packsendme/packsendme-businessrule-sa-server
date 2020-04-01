a26b9bcf8a8073641fdb6855fa02fd802de917b4
ricardomarzochi@gmail.com <r.libera.marzochi@accenture.com>
2020-04-01 10:14:12 +0200

    NewVersion-Release/0.2.0

--- src/main/java/com/packsendme/microservice/sa/businessrule/controller/Execute_Controller.java
+++ src/main/java/com/packsendme/microservice/sa/businessrule/controller/Execute_Controller.java
@@ -7,16 +7,12 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.packsendme.execution.bre.rule.model.ExecutionBRE_Model;
 import com.packsendme.microservice.sa.businessrule.service.ConsumerBRE_Service;
 import com.packsendme.microservice.sa.businessrule.service.ProduceBRE_Service;
 
-@RestController
-@RequestMapping("/businessrule/sa/execute")
 public class Execute_Controller {
 
 	
