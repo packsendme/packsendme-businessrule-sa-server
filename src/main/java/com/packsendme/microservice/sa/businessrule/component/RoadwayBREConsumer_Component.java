c600fdaef18ef8e97b2583fd3138264d7df261d6
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2020-04-01 11:56:18 +0200

    NewVersion

--- src/main/java/com/packsendme/microservice/sa/businessrule/component/RoadwayBREConsumer_Component.java
+++ src/main/java/com/packsendme/microservice/sa/businessrule/component/RoadwayBREConsumer_Component.java
@@ -8,7 +8,7 @@ public class RoadwayBREConsumer_Component implements BRE_Consumer {
 
 	private String msg;
 	
-	@KafkaListener(topics = "${kafka.topic.roadwayBRE}")
+	@KafkaListener(topics = "${kafka.topic.roadwayBRE_SA_Instance}")
 	public void receive(String data) {
 		this.msg = data;
 		System.out.println(" ------------------------------- ");
