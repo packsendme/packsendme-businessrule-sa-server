f434a90cc2987d0566742b2b0cd397c55feffba0
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2020-04-07 21:11:12 +0200

    NewVersion/0.2.0

--- src/main/java/com/packsendme/microservice/sa/businessrule/dao/ExecutionImpl_DAO.java
+++ src/main/java/com/packsendme/microservice/sa/businessrule/dao/ExecutionImpl_DAO.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class ExecutionImpl_DAO<T> implements IBusinessRule_DAO<T>{
 
+	@Autowired
 	private RedisTemplate<String, T> redisTemplate;
 
 	@Override
--- src/main/java/com/packsendme/microservice/sa/businessrule/dao/MaritimewayImpl_DAO.java
+++ src/main/java/com/packsendme/microservice/sa/businessrule/dao/MaritimewayImpl_DAO.java
@@ -1,44 +1,33 @@
 package com.packsendme.microservice.sa.businessrule.dao;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
-
-import org.springframework.data.redis.core.RedisTemplate;
 
 import com.packsendme.maritimeway.bre.rule.model.MaritimewayBRE_Model;
 
 public class MaritimewayImpl_DAO implements IBusinessRule_DAO<MaritimewayBRE_Model> {
 
-	private RedisTemplate<String, MaritimewayBRE_Model> redisTemplate;
-
 	@Override
 	public void add(MaritimewayBRE_Model object) {
-		redisTemplate.opsForValue().set(object.id_rule, object);
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
 	public void delete(String key) {
-		redisTemplate.opsForValue().getOperations().delete(key);
+		// TODO Auto-generated method stub
+		
 	}
 
-	
 	@Override
-	public MaritimewayBRE_Model findOne(String key) {
-		return redisTemplate.opsForValue().get(key);
+	public MaritimewayBRE_Model findOne(String id) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
 	public List<MaritimewayBRE_Model> findAll(String id) {
-		List<MaritimewayBRE_Model> roadwayL = new ArrayList<MaritimewayBRE_Model>();
-		Set<String> keys = redisTemplate.keys("*");
-		Iterator<String> it = keys.iterator();
-			
-		while(it.hasNext()){
-			roadwayL.add(findOne(it.next()));
-		}
-		return roadwayL;
+		// TODO Auto-generated method stub
+		return null;
 	}
- 
+
 }
