6b2a026a759803287f52a36fb83f64640f4eec07
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2020-04-08 10:05:04 +0200

    NewVersion/0.2.0

--- src/main/java/com/packsendme/microservice/sa/businessrule/dao/BusinessRuleImpl_DAO.java
+++ src/main/java/com/packsendme/microservice/sa/businessrule/dao/BusinessRuleImpl_DAO.java
@@ -10,7 +10,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ExecutionImpl_DAO<T> implements IBusinessRule_DAO<T>{
+public class BusinessRuleImpl_DAO<T> implements IBusinessRule_DAO<T>{
 
 	@Autowired
 	private RedisTemplate<String, T> redisTemplate;
