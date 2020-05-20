0324eedec0415933864b75bfc20a5c8a1d5a408b
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2020-05-20 20:04:47 +0200

    NewVersion/0.2.0

--- src/main/java/com/packsendme/microservice/sa/businessrule/controller/BusinessRuleBRE_Controller.java
+++ src/main/java/com/packsendme/microservice/sa/businessrule/controller/BusinessRuleBRE_Controller.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,6 +33,7 @@ public class BusinessRuleBRE_Controller {
 	// METHOD POST|GET :: EXECUTION-BRE
 	//========================================================================================//
 
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@PostMapping("/execution")
 	public ResponseEntity<?> postExecutionBRE_SA(@Validated @RequestBody ExecutionBRE_Model object) {		
 		try {
@@ -42,6 +44,7 @@ public class BusinessRuleBRE_Controller {
 		}
 	}
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@GetMapping("/execution/{name_rule}")
 	public ResponseEntity<?> getExecutionBRE_SA(@Validated  @PathVariable ("name_rule") String name_rule) {		
 		try {
@@ -52,6 +55,7 @@ public class BusinessRuleBRE_Controller {
 		}
 	}
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@DeleteMapping("/execution/{name_rule}")
 	public ResponseEntity<?> deleteExecutionBRE_SA(@Validated  @PathVariable ("name_rule") String name_rule) {		
 		try {
@@ -67,6 +71,7 @@ public class BusinessRuleBRE_Controller {
 	// METHOD POST|GET :: ROADWAY-BRE
 	//========================================================================================//
 
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@PostMapping("/roadway")
 	public ResponseEntity<?> postRoadwayBRE_SA(@Validated @RequestBody RoadwayBRE_Model object) {		
 		try {
@@ -79,6 +84,7 @@ public class BusinessRuleBRE_Controller {
 		}
 	}
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@GetMapping("/roadway/{name_rule}")
 	public ResponseEntity<?> getRoadwayBRE_SA(@Validated  @PathVariable ("name_rule") String name_rule) {		
 		try {
@@ -89,6 +95,7 @@ public class BusinessRuleBRE_Controller {
 		}
 	}
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@DeleteMapping("/roadway/{name_rule}")
 	public ResponseEntity<?> deleteRoadwayBRE_SA(@Validated  @PathVariable ("name_rule") String name_rule) {		
 		try {
@@ -103,6 +110,7 @@ public class BusinessRuleBRE_Controller {
 	// METHOD POST|GET :: AIRWAY-BRE
 	//========================================================================================//
 
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@PostMapping("/airway")
 	public ResponseEntity<?> postAirwayBRE_SA(@Validated @RequestBody AirwayBRE_Model object) {		
 		try {
@@ -113,6 +121,7 @@ public class BusinessRuleBRE_Controller {
 		}
 	}
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@GetMapping("/airway/{name_rule}")
 	public ResponseEntity<?> getAirwayBRE_SA(@Validated @PathVariable ("name_rule") String name_rule) {		
 		try {
@@ -123,6 +132,7 @@ public class BusinessRuleBRE_Controller {
 		}
 	}
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@DeleteMapping("/airway/{name_rule}")
 	public ResponseEntity<?> deleteAirwayBRE_SA(@Validated @PathVariable ("name_rule") String name_rule) {		
 		try {
@@ -137,6 +147,7 @@ public class BusinessRuleBRE_Controller {
 	// METHOD POST|GET :: MARITIMEWAY-BRE
 	//========================================================================================//
 
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@PostMapping("/maritimeway")
 	public ResponseEntity<?> postMaritimewayBRE_SA(@Validated @RequestBody MaritimewayBRE_Model object) {		
 		try {
@@ -147,6 +158,7 @@ public class BusinessRuleBRE_Controller {
 		}
 	}
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@GetMapping("/maritimeway/{name_rule}")
 	public ResponseEntity<?> getMaritimewayBRE_SA(@Validated @PathVariable ("name_rule") String name_rule) {		
 		try {
@@ -157,6 +169,7 @@ public class BusinessRuleBRE_Controller {
 		}
 	}
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@DeleteMapping("/maritimeway/{name_rule}")
 	public ResponseEntity<?> deleteMaritimewayBRE_SA(@Validated @PathVariable ("name_rule") String name_rule) {		
 		try {
@@ -171,6 +184,7 @@ public class BusinessRuleBRE_Controller {
 	// METHOD POST|GET :: TOLLSFUEL-BRE
 	//========================================================================================//
 
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@PostMapping("/tollsfuel")
 	public ResponseEntity<?> postTollsFuelBRE_SA(@Validated @RequestBody TollsFuelBRE_Model object) {		
 		try {
@@ -181,6 +195,7 @@ public class BusinessRuleBRE_Controller {
 		}
 	}
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@GetMapping("/tollsfuel/{name_rule}")
 	public ResponseEntity<?> getTollsFuelBRE_SA(@Validated @PathVariable ("name_rule") String name_rule) {		
 		try {
@@ -191,6 +206,7 @@ public class BusinessRuleBRE_Controller {
 		}
 	}
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@DeleteMapping("/tollsfuel/{name_rule}")
 	public ResponseEntity<?> deleteTollsFuelBRE_SA(@Validated @PathVariable ("name_rule") String name_rule) {		
 		try {
@@ -205,34 +221,36 @@ public class BusinessRuleBRE_Controller {
 		// METHOD POST|GET :: FinanceCostDelivery-BRE
 		//========================================================================================//
 
-		@PostMapping("/financecostdelivery")
-		public ResponseEntity<?> postFinanceCostDeliveryBRE_SA(@Validated @RequestBody FinanceCostDeliveryBRE_Model object) {		
-			try {
-				return businessRule_Services.financeCostDeliveryOperation(null,object, "POST");
-			} catch (Exception e) {
-				e.printStackTrace();
-				return new ResponseEntity<>(object, HttpStatus.INTERNAL_SERVER_ERROR);
-			}
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
+	@PostMapping("/financecostdelivery")
+	public ResponseEntity<?> postFinanceCostDeliveryBRE_SA(@Validated @RequestBody FinanceCostDeliveryBRE_Model object) {		
+		try {
+			return businessRule_Services.financeCostDeliveryOperation(null,object, "POST");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>(object, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
+	}
 		
-		@GetMapping("/financecostdelivery/{rule_type}")
-		public ResponseEntity<?> getFinanceCostDeliveryBRE_SA(@Validated @PathVariable ("rule_type") String rule_type) {		
-			try {
-				return businessRule_Services.financeCostDeliveryOperation(rule_type,null,"GET");
-			} catch (Exception e) {
-				e.printStackTrace();
-				return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-			}
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
+	@GetMapping("/financecostdelivery/{rule_type}")
+	public ResponseEntity<?> getFinanceCostDeliveryBRE_SA(@Validated @PathVariable ("rule_type") String rule_type) {		
+		try {
+			return businessRule_Services.financeCostDeliveryOperation(rule_type,null,"GET");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		
-		@DeleteMapping("/financecostdelivery/{rule_type}")
-		public ResponseEntity<?> deleteFinanceCostDeliveryBRE_SA(@Validated @PathVariable ("rule_type") String rule_type) {		
-			try {
-				return businessRule_Services.financeCostDeliveryOperation(rule_type,null,"DELETE");
-			} catch (Exception e) {
-				e.printStackTrace();
-				return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-			}
-		}
+	}
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
+	@DeleteMapping("/financecostdelivery/{rule_type}")
+	public ResponseEntity<?> deleteFinanceCostDeliveryBRE_SA(@Validated @PathVariable ("rule_type") String rule_type) {		
+		try {
+			return businessRule_Services.financeCostDeliveryOperation(rule_type,null,"DELETE");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 }
