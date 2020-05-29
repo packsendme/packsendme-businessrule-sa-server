0ca52923997501ead04f0ba45f654ad731a9af11
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2020-05-29 15:18:50 +0200

    NewVersion/0.2.0

--- src/main/java/com/packsendme/microservice/sa/businessrule/component/TollsFuelConvertCurrent.java
+++ src/main/java/com/packsendme/microservice/sa/businessrule/component/TollsFuelConvertCurrent.java
@@ -2,19 +2,22 @@ package com.packsendme.microservice.sa.businessrule.component;
 
 import org.springframework.stereotype.Component;
 
+import com.packsendme.lib.utility.FormatValueMoney;
 import com.packsendme.tollsfuel.bre.model.TollsFuelBRE_Model;
 import com.packsendme.tollsfuel.bre.rule.price.model.TollsFuelPriceCountryBRE_Model;
 
 @Component
 public class TollsFuelConvertCurrent {
 	
+	FormatValueMoney moneyFormat = new FormatValueMoney();
+	
 	// Execute to GET operation	
 	public TollsFuelBRE_Model opGETConvertToCountryOrigin(TollsFuelBRE_Model tollsFuelBRE, String country) {
 			
 		TollsFuelPriceCountryBRE_Model tollsfuelPriceCountry = tollsFuelBRE.tollsfuelPriceCountry.get(country);
 		
-		tollsfuelPriceCountry.fuel_price = tollsfuelPriceCountry.fuel_price * tollsfuelPriceCountry.rate_exchange;
-		tollsfuelPriceCountry.tolls_price = tollsfuelPriceCountry.tolls_price * tollsfuelPriceCountry.rate_exchange;
+		tollsfuelPriceCountry.fuel_price = moneyFormat.doubleFormatRound(tollsfuelPriceCountry.fuel_price * tollsfuelPriceCountry.rate_exchange);
+		tollsfuelPriceCountry.tolls_price = moneyFormat.doubleFormatRound(tollsfuelPriceCountry.tolls_price * tollsfuelPriceCountry.rate_exchange);
 		// Add Map Way by Country
 		tollsFuelBRE.tollsfuelPriceCountry.put(country, tollsfuelPriceCountry);
 		return tollsFuelBRE;
@@ -25,8 +28,8 @@ public class TollsFuelConvertCurrent {
 		
 		TollsFuelPriceCountryBRE_Model tollsfuelPriceCountry = tollsFuelBRE.tollsfuelPriceCountry.get(country);
 		
-		tollsfuelPriceCountry.fuel_price = tollsfuelPriceCountry.fuel_price / vlr_rate;
-		tollsfuelPriceCountry.tolls_price = tollsfuelPriceCountry.tolls_price / vlr_rate;
+		tollsfuelPriceCountry.fuel_price = moneyFormat.doubleFormatRound(tollsfuelPriceCountry.fuel_price / vlr_rate);
+		tollsfuelPriceCountry.tolls_price = moneyFormat.doubleFormatRound(tollsfuelPriceCountry.tolls_price / vlr_rate);
 		tollsfuelPriceCountry.rate_exchange = vlr_rate;
 		// Add Map Way by Country
 		tollsFuelBRE.tollsfuelPriceCountry.put(country, tollsfuelPriceCountry);
